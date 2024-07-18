ef977bd0561968d82f8e200c6d9454dc875d1b93
i_haiduk <ivannahaiduk@gmail.com>
2024-07-18 16:28:07 +0200

    added login rout page

--- frontend/routes/users.js
+++ frontend/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+      
+   fetch('http://localhost:3000/users')
+          .then(res => res.json())
+          .then(response => {
+              res.render('users', { title: 'Login page' });
+              }); 
+  });
+          
 
 module.exports = router;
